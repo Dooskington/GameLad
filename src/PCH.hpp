7a3ee7fa41886e6aff28a3815eb487fb33fd6aa2
Shane DeSeranno <tyren@syntheticstream.com>
2015-10-21 08:52:48 -0700

    Try using std lib to manage logger formatting.

--- src/PCH.hpp
+++ src/PCH.hpp
@@ -3,20 +3,15 @@
 #include <string>
 #include <iostream>
 
-#include <stdarg.h>
+#include <cstdarg>
 #include <memory>
 
 #if WINDOWS
     #include <SDL.h>
     #include <SDL_mixer.h>
-
-    #define vsnprintf _vsnprintf
 #else
     #include "SDL2/SDL.h"
     #include "SDL2_mixer/SDL_mixer.h"
-
-    // TODO: Doosk to add the correct #define macro here to map to OSX's printf if necessary
-    //#define vsnprintf _vsnprintf???
 #endif
 
 #include "Logger.hpp"
