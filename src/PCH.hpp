8f43ea6bad55c436767c5e3e42df6af5c5d4df03
Declan Hopkins <hopkins.declan@gmail.com>
2015-10-21 23:10:00 -0400

    Remove SDL_mixer for now

--- src/PCH.hpp
+++ src/PCH.hpp
@@ -8,10 +8,10 @@
 
 #if WINDOWS
     #include <SDL.h>
-    #include <SDL_mixer.h>
+   // #include <SDL_mixer.h>
 #else
     #include "SDL2/SDL.h"
-    #include "SDL2_mixer/SDL_mixer.h"
+   // #include "SDL2_mixer/SDL_mixer.h"
 #endif
 
 #include "Logger.hpp"
