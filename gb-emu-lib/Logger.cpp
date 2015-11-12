f06ed8d5a2b4e6c4dc08153f85428d6199a6308f
Shane DeSeranno <tyren@syntheticstream.com>
2015-11-12 12:47:48 -0800

    Oops, forgot the logger changes.

--- gb-emu-lib/Logger.cpp
+++ gb-emu-lib/Logger.cpp
@@ -1,30 +1,26 @@
-#include "PCH.hpp"
+#include "pch.hpp"
 #include "Logger.hpp"
 
-void Logger::Log(const std::string message, ...)
+void Logger::Log(const char* message, ...)
 {
     va_list argPointer;
     va_start(argPointer, message);
 
-    // Get the size required so we can allocate the correct space
-    size_t size = std::vsnprintf(nullptr, 0, message.c_str(), argPointer) + 1;
-    std::unique_ptr<char[]> buf(new char[size]);
-    std::vsnprintf(&buf.get()[0], size, message.c_str(), argPointer);
+    char buffer[1024];
+    std::vsnprintf(buffer, ARRAYSIZE(buffer), message, argPointer);
 
     va_end(argPointer);
-    std::cout << buf.get() << std::endl;
+    std::cout << buffer << std::endl;
 }
 
-void Logger::LogError(const std::string message, ...)
+void Logger::LogError(const char* message, ...)
 {
     va_list argPointer;
     va_start(argPointer, message);
 
-    // Get the size required so we can allocate the correct space
-    size_t size = std::vsnprintf(nullptr, 0, message.c_str(), argPointer) + 1;
-    std::unique_ptr<char[]> buf(new char[size]);
-    std::vsnprintf(&buf.get()[0], size, message.c_str(), argPointer);
+    char buffer[1024];
+    std::vsnprintf(buffer, ARRAYSIZE(buffer), message, argPointer);
 
     va_end(argPointer);
-    std::cerr << buf.get() << std::endl;
+    std::cerr << buffer << std::endl;
 }
--- gb-emu-lib/Logger.hpp
+++ gb-emu-lib/Logger.hpp
@@ -6,6 +6,6 @@ private:
     Logger() { }
 
 public:
-    static void Log(const std::string message, ...);
-    static void LogError(const std::string message, ...);
+    static void Log(const char* message, ...);
+    static void LogError(const char* message, ...);
 };
