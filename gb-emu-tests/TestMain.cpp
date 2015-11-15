0171370ab5fd00a7acc436bf00d659cd20c75f59
Declan Hopkins <hopkins.declan@gmail.com>
2015-11-14 21:46:45 -0500

    Add missing tests for linux/osx

--- gb-emu-tests/TestMain.cpp
+++ gb-emu-tests/TestMain.cpp
@@ -62,6 +62,9 @@ int main(int arg, char** argv)
     TEST_CALL(CPUTests, INCH_Test);
     TEST_CALL(CPUTests, INCL_Test);
     TEST_CALL(CPUTests, INCBC_Test);
+    TEST_CALL(CPUTests, INCDE_Test);
+    TEST_CALL(CPUTests, INCHL_Test);
+    TEST_CALL(CPUTests, INCSP_Test);
     TEST_CALL(CPUTests, DECB_Test);
     TEST_CALL(CPUTests, DECC_Test);
     TEST_CALL(CPUTests, DECD_Test);
