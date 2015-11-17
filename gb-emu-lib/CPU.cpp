d93e4b35b219ee2e3bc3b706cbddbad83d39d23a
Shane DeSeranno <tyren@syntheticstream.com>
2015-11-17 13:20:43 -0800

    Fix SRAr

--- gb-emu-lib/CPU.cpp
+++ gb-emu-lib/CPU.cpp
@@ -3377,7 +3377,7 @@ void CPU::SRAr(const byte& opCode)
     ISBITSET(*r, 0) ? SetFlag(CarryFlag) : ClearFlag(CarryFlag);
 
     // Shift r right
-    (*r) = *r >> 1;
+    (*r) = (*r >> 1) | (*r & 0x80);
 
     // Affects Z, clears N, clears H, affects C
     (*r == 0x00) ? SetFlag(ZeroFlag) : ClearFlag(ZeroFlag);
